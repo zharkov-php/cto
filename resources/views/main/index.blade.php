f929025c8c06ebed5a547353d6ea4852dff1fabb
Zharkov Andrey <zharkov.php@gmail.com>
2018-10-15 18:04:50 +0300

    main.template.content

--- resources/views/main/index.blade.php
+++ resources/views/main/index.blade.php
@@ -16,15 +16,15 @@
                     <div class="inner-box">
                         <div class="image-box">
                             <figure><img src="/main_template/images/resource/service-1.jpg" alt=""></figure>
-                            <div class="title"><h4>Suspension Repair</h4></div>
+                            <div class="title"><h4>Ремонт ходовой системы</h4></div>
                         </div>
                         <div class="caption-box">
                             <div class="title-box">
                                 <span class="icon flaticon-electrical"></span>
-                                <h4><a href="/">Suspension Repair</a></h4>
+                                <h4><a href="/">Ремонт ходовой системы</a></h4>
                             </div>
-                            <p>We should point out that maintaining your vehicle with an occasional visual inspectionfluid level.</p>
-                            <a href="/" class="read-more">Read More <i class="fa fa-angle-double-right"></i></a>
+                            <p>Производим диагностику и ремонт ходовой системы любой сложности.
+                                Используем как оригинальные запчасти та и аналоги, по запросу клиента</p>
                         </div>
                     </div>
                 </div>
@@ -34,15 +34,14 @@
                     <div class="inner-box">
                         <div class="image-box">
                             <figure><img src="/main_template/images/resource/service-2.jpg" alt=""></figure>
-                            <div class="title"><h4>Engine Overhaul</h4></div>
+                            <div class="title"><h4>Диагностика</h4></div>
                         </div>
                         <div class="caption-box">
                             <div class="title-box">
                                 <span class="icon flaticon-pistons"></span>
-                                <h4><a href="/">Engine Overhaul</a></h4>
+                                <h4><a href="/">Диагностика</a></h4>
                             </div>
-                            <p>We should point out that maintaining your vehicle with an occasional visual inspectionfluid level.</p>
-                            <a href="/" class="read-more">Read More <i class="fa fa-angle-double-right"></i></a>
+                            <p>На наше станции мы проводим компьютерную диагностику Вашего автомобиля, снятие ошибок, настройка системы.</p>
                         </div>
                     </div>
                 </div>
@@ -52,15 +51,14 @@
                     <div class="inner-box">
                         <div class="image-box">
                             <figure><img src="/main_template/images/resource/service-3.jpg" alt=""></figure>
-                            <div class="title"><h4>Wheel Alignment</h4></div>
+                            <div class="title"><h4>Шиномонтаж</h4></div>
                         </div>
                         <div class="caption-box">
                             <div class="title-box">
                                 <span class="icon flaticon-cogwheel"></span>
-                                <h4><a href="/">Wheel Alignment</a></h4>
+                                <h4><a href="/">Шиномонтаж</a></h4>
                             </div>
-                            <p>We should point out that maintaining your vehicle with an occasional visual inspectionfluid level.</p>
-                            <a href="/" class="read-more">Read More <i class="fa fa-angle-double-right"></i></a>
+                            <p>На нашей станции мы делаем шиномонтаж и балансировку колёс.</p>
                         </div>
                     </div>
                 </div>
@@ -69,16 +67,32 @@
                 <div class="service-block">
                     <div class="inner-box">
                         <div class="image-box">
-                            <figure><img src="/main_template/images/resource/service-1.jpg" alt=""></figure>
-                            <div class="title"><h4>Suspension Repair</h4></div>
+                            <figure><img src="/main_template/images/resource/service-4.jpg" alt=""></figure>
+                            <div class="title"><h4>Ремонт топливной системы</h4></div>
                         </div>
                         <div class="caption-box">
                             <div class="title-box">
                                 <span class="icon flaticon-electrical"></span>
-                                <h4><a href="/">Suspension Repair</a></h4>
+                                <h4><a href="/">Ремонт топливной системы</a></h4>
                             </div>
-                            <p>We should point out that maintaining your vehicle with an occasional visual inspectionfluid level.</p>
-                            <a href="/" class="read-more">Read More <i class="fa fa-angle-double-right"></i></a>
+                            <p>Осуществляем ремонт топливных систем как в бензиновых автомобилях, так и  в дизельных.</p>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Service Block -->
+                <div class="service-block">
+                    <div class="inner-box">
+                        <div class="image-box">
+                            <figure><img src="/main_template/images/resource/service-6.jpg" alt=""></figure>
+                            <div class="title"><h4>Техническое обслуживание</h4></div>
+                        </div>
+                        <div class="caption-box">
+                            <div class="title-box">
+                                <span class="icon flaticon-pistons"></span>
+                                <h4><a href="/">Техническое обслуживание</a></h4>
+                            </div>
+                            <p>Производим замену фильтров всех систем, а так же масел, жидкостей быстро и качественно.</p>
                         </div>
                     </div>
                 </div>
@@ -88,90 +102,20 @@
                     <div class="inner-box">
                         <div class="image-box">
                             <figure><img src="/main_template/images/resource/service-2.jpg" alt=""></figure>
-                            <div class="title"><h4>Engine Overhaul</h4></div>
-                        </div>
-                        <div class="caption-box">
-                            <div class="title-box">
-                                <span class="icon flaticon-pistons"></span>
-                                <h4><a href="/">Engine Overhaul</a></h4>
-                            </div>
-                            <p>We should point out that maintaining your vehicle with an occasional visual inspectionfluid level.</p>
-                            <a href="/" class="read-more">Read More <i class="fa fa-angle-double-right"></i></a>
-                        </div>
-                    </div>
-                </div>
-
-                <!-- Service Block -->
-                <div class="service-block">
-                    <div class="inner-box">
-                        <div class="image-box">
-                            <figure><img src="/main_template/images/resource/service-3.jpg" alt=""></figure>
-                            <div class="title"><h4>Wheel Alignment</h4></div>
+                            <div class="title"><h4> Капитальный ремонт двигателя</h4></div>
                         </div>
                         <div class="caption-box">
                             <div class="title-box">
                                 <span class="icon flaticon-cogwheel"></span>
-                                <h4><a href="/">Wheel Alignment</a></h4>
+                                <h4><a href="/"> Капитальный ремонт двигателя</a></h4>
                             </div>
-                            <p>We should point out that maintaining your vehicle with an occasional visual inspectionfluid level.</p>
-                            <a href="/" class="read-more">Read More <i class="fa fa-angle-double-right"></i></a>
+                            <p>Наши специалисты осуществляют ремонт как бензиновых, та дизельных двигателей.
+                                Делаем диагностику, снятие, дефектовку, ремонт, установку двигателей.
+                                Используем как наши материалы так и материалы клиента.</p>
                         </div>
                     </div>
                 </div>
 
-                <!-- Service Block -->
-                <div class="service-block">
-                    <div class="inner-box">
-                        <div class="image-box">
-                            <figure><img src="/main_template/images/resource/service-1.jpg" alt=""></figure>
-                            <div class="title"><h4>Suspension Repair</h4></div>
-                        </div>
-                        <div class="caption-box">
-                            <div class="title-box">
-                                <span class="icon flaticon-electrical"></span>
-                                <h4><a href="/">Suspension Repair</a></h4>
-                            </div>
-                            <p>We should point out that maintaining your vehicle with an occasional visual inspectionfluid level.</p>
-                            <a href="/" class="read-more">Read More <i class="fa fa-angle-double-right"></i></a>
-                        </div>
-                    </div>
-                </div>
-
-                <!-- Service Block -->
-                <div class="service-block">
-                    <div class="inner-box">
-                        <div class="image-box">
-                            <figure><img src="/main_template/images/resource/service-2.jpg" alt=""></figure>
-                            <div class="title"><h4>Engine Overhaul</h4></div>
-                        </div>
-                        <div class="caption-box">
-                            <div class="title-box">
-                                <span class="icon flaticon-pistons"></span>
-                                <h4><a href="/">Engine Overhaul</a></h4>
-                            </div>
-                            <p>We should point out that maintaining your vehicle with an occasional visual inspectionfluid level.</p>
-                            <a href="/" class="read-more">Read More <i class="fa fa-angle-double-right"></i></a>
-                        </div>
-                    </div>
-                </div>
-
-                <!-- Service Block -->
-                <div class="service-block">
-                    <div class="inner-box">
-                        <div class="image-box">
-                            <figure><img src="/main_template/images/resource/service-3.jpg" alt=""></figure>
-                            <div class="title"><h4>Wheel Alignment</h4></div>
-                        </div>
-                        <div class="caption-box">
-                            <div class="title-box">
-                                <span class="icon flaticon-cogwheel"></span>
-                                <h4><a href="/">Wheel Alignment</a></h4>
-                            </div>
-                            <p>We should point out that maintaining your vehicle with an occasional visual inspectionfluid level.</p>
-                            <a href="/" class="read-more">Read More <i class="fa fa-angle-double-right"></i></a>
-                        </div>
-                    </div>
-                </div>
             </div>
         </div>
     </section>
