3e798e58827f7388a7f82bfb065f4f8781ec6b30
Zharkov Andrey <zharkov.php@gmail.com>
2018-10-15 17:34:52 +0300

    service.template.content

--- resources/views/service/index.blade.php
+++ resources/views/service/index.blade.php
@@ -13,14 +13,15 @@
                     <div class="inner-box">
                         <div class="image-box">
                             <figure><img src="/main_template/images/resource/service-1.jpg" alt=""></figure>
-                            <div class="title"><h4>Ремонт подвески</h4></div>
+                            <div class="title"><h4>Ремонт ходовой системы</h4></div>
                         </div>
                         <div class="caption-box">
                             <div class="title-box">
                                 <span class="icon flaticon-electrical"></span>
-                                <h4><a href="service-detail.html">Ремонт подвески</a></h4>
+                                <h4><a href="service-detail.html">Ремонт ходовой системы</a></h4>
                             </div>
-                            <p>We should point out that maintaining your vehicle with an occasional visual inspectionfluid level.</p>
+                            <p>Производим диагностику и ремонт ходовой системы любой сложности.
+                                Используем как оригинальные запчасти та и аналоги, по запросу клиента</p>
                         </div>
                     </div>
                 </div>
@@ -37,7 +38,9 @@
                                 <span class="icon flaticon-pistons"></span>
                                 <h4><a href="service-detail.html">Капитальный ремонт двигателя</a></h4>
                             </div>
-                            <p>We should point out that maintaining your vehicle with an occasional visual inspectionfluid level.</p>
+                            <p>Наши специалисты осуществляют ремонт как бензиновых, та дизельных двигателей.
+                                Делаем диагностику, снятие, дефектовку, ремонт, установку двигателей.
+                                Используем как наши материалы так и материалы клиента.</p>
                         </div>
                     </div>
                 </div>
@@ -47,14 +50,15 @@
                     <div class="inner-box">
                         <div class="image-box">
                             <figure><img src="/main_template/images/resource/service-3.jpg" alt=""></figure>
-                            <div class="title"><h4>Шото мля... и с колесами делаем)))</h4></div>
+                            <div class="title"><h4>Шиномонтаж</h4></div>
                         </div>
                         <div class="caption-box">
                             <div class="title-box">
                                 <span class="icon flaticon-cogwheel"></span>
-                                <h4><a href="service-detail.html">Wheel Alignment</a></h4>
+                                <h4><a href="service-detail.html">Шиномонтаж</a></h4>
                             </div>
-                            <p>We should point out that maintaining your vehicle with an occasional visual inspectionfluid level.</p>
+                            <p>
+</p>
                         </div>
                     </div>
                 </div>
@@ -64,14 +68,14 @@
                     <div class="inner-box">
                         <div class="image-box">
                             <figure><img src="/main_template/images/resource/service-4.jpg" alt=""></figure>
-                            <div class="title"><h4>Что то умеем открутить</h4></div>
+                            <div class="title"><h4>Ремонт топливной системы</h4></div>
                         </div>
                         <div class="caption-box">
                             <div class="title-box">
                                 <span class="icon flaticon-electrical"></span>
-                                <h4><a href="service-detail.html">Suspension Repair</a></h4>
+                                <h4><a href="service-detail.html">Ремонт топливной системы</a></h4>
                             </div>
-                            <p>We should point out that maintaining your vehicle with an occasional visual inspectionfluid level.</p>
+                            <p>Осуществляем ремонт топливных систем как в бензиновых автомобилях, так и  в дизельных.</p>
                         </div>
                     </div>
                 </div>
@@ -81,14 +85,14 @@
                     <div class="inner-box">
                         <div class="image-box">
                             <figure><img src="/main_template/images/resource/service-5.jpg" alt=""></figure>
-                            <div class="title"><h4>Порно смотрим на работе</h4></div>
+                            <div class="title"><h4>Диагностика</h4></div>
                         </div>
                         <div class="caption-box">
                             <div class="title-box">
                                 <span class="icon flaticon-pistons"></span>
-                                <h4><a href="service-detail.html">Порно смотрим на работе</a></h4>
+                                <h4><a href="service-detail.html">Диагностика</a></h4>
                             </div>
-                            <p>We should point out that maintaining your vehicle with an occasional visual inspectionfluid level.</p>
+                            <p>На наше станции мы проводим компьютерную диагностику Вашего автомобиля, снятие ошибок, настройка системы.</p>
                         </div>
                     </div>
                 </div>
@@ -98,20 +102,20 @@
                     <div class="inner-box">
                         <div class="image-box">
                             <figure><img src="/main_template/images/resource/service-6.jpg" alt=""></figure>
-                            <div class="title"><h4>Находим лишние детали</h4></div>
+                            <div class="title"><h4>Техническое обслуживание </h4></div>
                         </div>
                         <div class="caption-box">
                             <div class="title-box">
                                 <span class="icon flaticon-cogwheel"></span>
-                                <h4><a href="service-detail.html">Находим лишние детали</a></h4>
+                                <h4><a href="service-detail.html">Техническое обслуживание </a></h4>
                             </div>
-                            <p>We should point out that maintaining your vehicle with an occasional visual inspectionfluid level.</p>
+                            <p>Производим замену фильтров всех систем, а так же масел, жидкостей быстро и качественно.</p>
                         </div>
                     </div>
                 </div>
             </div>
 
-            <!-- Styled Pagination -->
+          {{--  <!-- Styled Pagination -->
             <div class="styled-pagination text-center clearfix">
                 <ul class="clearfix">
                     <li><a href="#" class="active">1</a></li>
@@ -120,7 +124,7 @@
                     <li><a href="#">4</a></li>
                     <li><a class="next" href="#"><span class="fa fa-angle-right"></span></a></li>
                 </ul>
-            </div>
+            </div>--}}
         </div>
     </section>
     <!-- End Services Section -->
